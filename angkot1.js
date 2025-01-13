6269bb9751dfd39c5d4922e51a57c3eb0be31e5b
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-13 23:10:21 +0700

    for js

--- angkot1.js
+++ angkot1.js
@@ -1,5 +1,6 @@
-nilaiAwal = 1;
-while(nilaiAwal <= 10){
-    console.log('Angkot No. ' + nilaiAwal + ' beroperasi dengan baik');
-    nilaiAwal++;
+var jmlAngkot = 10;
+var noAngkot = 1;
+while(noAngkot <= jmlAngkot){
+    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik');
+    noAngkot++;
 }
