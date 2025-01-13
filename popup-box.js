d952a8474e4a232a6ea3051b567adcb28ca2cf00
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-13 22:27:05 +0700

    popup box

---
+++ popup-box.js
@@ -0,0 +1,17 @@
+// var nama = prompt('Masukkan nama');
+// alert(nama);
+// console.log(nama);
+
+// var tes = confirm('yakin?');
+// alert(tes);
+
+alert('Selamat datang');
+
+var tes = true;
+while(tes){
+    var nama = prompt('masukkan nama anda :');
+    alert('nama anda ' + nama);
+
+    tes = confirm('lanjut?');
+}
+alert('Terima kasih');
