7cf80d066ad90c2467b44afc0ba2bc119eb4ca3b
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-13 23:47:04 +0700

    pengkondisian

--- angkot6.js
+++ angkot6.js
@@ -2,9 +2,11 @@ var jmlAngkot = 10;
 var angkotBeroperasi = 6;
 
 for(var noAngkot = 1; noAngkot <= jmlAngkot  ; noAngkot++){
-    if(noAngkot <= angkotBeroperasi){
+    if(noAngkot <= angkotBeroperasi && noAngkot !== 5){
         console.log('Angkot No ' + noAngkot +' beroperasi dengan baik');
-    }  else {
+    } else if(noAngkot === 8 || noAngkot === 10 || noAngkot === 5) {
+        console.log('Angkot No ' + noAngkot + ' sedang lembur');
+    } else {
         console.log('Angkot No ' + noAngkot + ' sedang tidak beroperasi' );
     }
 }
