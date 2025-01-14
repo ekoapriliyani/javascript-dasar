693d93e28bb20e49c55ab3ad1434b8257198ab76
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-14 21:16:12 +0700

    pengulangan dan pengkondisian bersarang

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-var x = 10;
-    console.log('Hello World');
-    console.log('isi dari variable x adalah ' + x);
+// var x = 10;
+//     console.log('Hello World');
+//     console.log('isi dari variable x adalah ' + x);
+
+
+var s = '';
+
+for(var i = 0; i < 5; i++){
+    for(var j = 0; j <= i; j++){
+        s += '*';
+    }
+    s += '\n';
+}
+console.log(s);
