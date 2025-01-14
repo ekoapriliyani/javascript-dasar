b27217617d4ef4586c34be575fb5644bb75503af
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-14 20:59:19 +0700

    switch

--- switch.js
+++ switch.js
@@ -1,16 +1,38 @@
-var angka = prompt('masukkan angka :');
+// var angka = prompt('masukkan angka :');
 
-switch(angka) {
-    case '1' :
-        alert('anda memasukkan angka 1');
+// switch(angka) {
+//     case '1' :
+//         alert('anda memasukkan angka 1');
+//         break;
+//     case '2' :
+//         alert('anda memasukkan angka 2');
+//         break;
+//     case '3' :
+//         alert('anda memasukkan angka 3');
+//         break;
+//     default :
+//         alert('angka yang anda masukkan salah');
+//         break;
+// }
+
+var item = prompt('masukkan nama makanan / minuman : \n (cth: nasi, daging, susu, hamburger, softdrink');
+
+switch(item){
+    case 'nasi' :
+        alert('makanan / minuman SEHAT!');
         break;
-    case '2' :
-        alert('anda memasukkan angka 2');
+    case 'daging' :
+        alert('makanan / minuman SEHAT!');
         break;
-    case '3' :
-        alert('anda memasukkan angka 3');
+    case 'susu':
+        alert('makanan / minuman SEHAT!')
+        break;
+    case 'hamburger' :
+        alert('makanan / minuman TIDAK SEHAT!');
+        break
+    case 'softdrink' :
+        alert('makanan / minuman TIDAK SEHAT!');
         break;
     default :
-        alert('angka yang anda masukkan salah');
-        break;
+        alert('makanan / minuman yang anda masukkan salah!');
 }
