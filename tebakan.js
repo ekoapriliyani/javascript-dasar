f18bfb7793de7a8e55b3e5645853df6da13c4d5e
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-15 20:39:49 +0700

    game tebakan angka

--- tebakan.js
+++ tebakan.js
@@ -1,35 +1,36 @@
-// game tebakan angka
+// game tebka angka
+
 var lagi = true;
 
-while(lagi){
+while(lagi) {
+    var comp = Math.floor(Math.random()*10) + 1;
 
-    var comp = Math.floor(Math.random() * 10) + 1;
-    
     var kesempatan = 3;
 
     while(kesempatan > 0){
+
         if(kesempatan == 3){
-            alert('Selamat Datang di Game Tebakan Angka. \nAnda memiliki ' + kesempatan + ' kesempatan');
-        } else {
-            alert('kamu masih punya ' + kesempatan + ' kesempatan, ayo coba lagi!');
+            alert('Selamat Datang di Game Tebak Angka \nAnda memiliki 3 kesempatan');
+        } else{
+            alert('Anda masih punya ' + kesempatan + ' kesempatan');
         }
 
-        var p = prompt('pilih angka : 1 - 10');
+        var p = prompt('masukkan angka tebakanmu: 1-10');
 
         if(p == comp){
-            alert('KAMU BENAR');
+            alert('ANDA BENAR!');
             kesempatan = 0;
-        } else if(p < comp){
-            alert('Tebakannya terlalu rendah, silahkan coba lagi');
+        } else if(p > comp) {
+            alert('tebakan anda terlalu tinggi');
             kesempatan--;
         } else {
-            alert('Tebakannya terlalu tinggi, silahkan coba lagi');
+            alert('tebakan anda terlalu rendah');
             kesempatan--;
         }
     }
     alert('Jawabannya adalah ' + comp);
 
-    lagi = confirm('apakah kamu ingin mengulangi lagi?');
+    lagi = confirm('anda mau mengulangi permainan lagi?');
 }
 
-alert('terima kasih');
+alert('Terima Kasih');
