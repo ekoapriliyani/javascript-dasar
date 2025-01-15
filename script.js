ac83fab0a9d4e07cde7b58936632196601b401ee
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-15 21:35:50 +0700

    function

--- script.js
+++ script.js
@@ -3,12 +3,28 @@
 //     console.log('isi dari variable x adalah ' + x);
 
 
-var s = '';
+// var s = '';
 
-for(var i = 0; i < 5; i++){
-    for(var j = 0; j <= i; j++){
-        s += '*';
-    }
-    s += '\n';
+// for(var i = 0; i < 5; i++){
+//     for(var j = 0; j <= i; j++){
+//         s += '*';
+//     }
+//     s += '\n';
+// }
+// console.log(s);
+
+function jumlahVolumeDuaKubus(a,b) {
+    var volumeA;
+    var volumeB;
+    var total;
+
+    volumeA = a * a * a;
+    volumeB = b * b * b;
+
+    total = volumeA + volumeB;
+
+    return total;
 }
-console.log(s);
+
+alert(jumlahVolumeDuaKubus(8, 3));
+alert(jumlahVolumeDuaKubus(10, 15));
