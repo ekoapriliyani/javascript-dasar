22a00fa6a1a49d21f97a839d1304d40d1702de91
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-17 22:37:47 +0700

    forEach, map, sort

--- array.js
+++ array.js
@@ -42,6 +42,38 @@
 
 // 5. slice
 // slice(awal, akhir)
-var arr = ["Eko", "Apriliyani", "Maziyyatun", "Nisa"];
-var arr2 = arr.slice(1,3);
-console.log(arr2.join(' - '));
+// var arr = ["Eko", "Apriliyani", "Maziyyatun", "Nisa"];
+// var arr2 = arr.slice(1,3);
+// console.log(arr2.join(' - '));
+
+// 6. foreach
+// var angka = [1,2,3,4,5,6,7,8];
+// var nama = ["Eko", "Apriliyani", "Maziyyatun", "Nisa"];
+// angka.forEach(function(e) {
+//     console.log(e);
+// });
+
+// nama.forEach(function(e, i) {
+//     console.log('Mahasiswa ke-' + (i+1) + ' adalah : ' + e);
+// });
+
+
+// 7. map
+// var angka = [1,2,3,4,5,6,2,1];
+// var angka2 = angka.map(function(e) {
+//     return e * 2;
+// });
+// console.log(angka2.join(' - '));
+
+
+
+// 8. sort
+var angka = [1,2,3,4,5,6,2,1];
+angka.sort();
+console.log(angka);
+
+var angka2 = [2,3,10,1,20,5,2,6,7,8];
+angka2.sort(function(a,b){
+    return a-b;
+});
+console.log(angka2.join(' - '));
