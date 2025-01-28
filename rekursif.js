56e4d95b7542d49d74fcbcef789f820cffcb486b
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-28 14:40:05 +0700

    rekursif

--- rekursif.js
+++ rekursif.js
@@ -1,10 +1,18 @@
 function tampilAngka(n) {
+<<<<<<< Updated upstream
     if(n == 0) {
         return;
     }
     console.log(n);
     tampilAngka(n-1);
     
+=======
+    if(n === 0){
+        return;
+    }
+    console.log(n);
+    return tampilAngka(n-1);
+>>>>>>> Stashed changes
 }
 
 tampilAngka(10);
