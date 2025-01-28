1bca0e70eda133f2759d3709cd0d9f601fc09f92
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-28 15:03:35 +0700

    juragan angkot 2

--- juraganAngkot.js
+++ juraganAngkot.js
@@ -1,4 +1,32 @@
-var penumpang = [];
+var penumpang = ['Eko', undefined, 'Doddy'];
 var tambahPenumpang = function(namaPenumpang, penumpang){
-    
+    // jika angkot kosong
+    if(penumpang.length == 0) {
+        // tambah penumpang di awal array
+        penumpang.push(namaPenumpang);
+        // kembailkan isi array dan keluar dari function
+        return penumpang;
+    } else {
+        // telusuri seluruh kursi dari awal
+        for(var i = 0; i < penumpang.length; i++) {
+            // jika kursi kosong
+            if(penumpang[i] == undefined) {
+                // tambah penumpang di kursi tersebut
+                penumpang[i] = namaPenumpang;
+                // kembalikan isi array dan keluar dari function
+                return penumpang;
+            }
+            // jika sudah ada nama yang sama
+                // tampilkan pesan kesalahannya
+                // kembalikan isi array dan keluar dari function
+            // jika seluruh kursi terisi
+            else if(i == penumpang.length - 1) {
+                // tambah penumpang di akhir array
+                penumpang.push(namaPenumpang);
+                // kembalikan isi array dan keluar dari function
+                return penumpang;
+            }
+        }
+    }
+
 }
