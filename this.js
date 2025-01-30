bc6ed504e8833f81cfc6fcdeef2c3c4574941380
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-30 14:31:40 +0700

    this js

---
+++ this.js
@@ -0,0 +1,38 @@
+// membuat object
+
+// cara 1 - function declaration
+// function halo(){
+//     console.log(this);
+//     console.log('halo');
+// }
+// this.halo();
+// this mengembalikan object Global
+
+
+
+// cara 2 - object literal
+// var obj = {a : 10, nama : 'Eko'};
+// obj.halo = function() {
+//     console.log(this);
+//     console.log('halo');
+// }
+// obj.halo();
+// this mengembalikan object yang bersangkutan
+
+
+
+// cara 3 - constructor
+function Halo() {
+    console.log(this);
+    console.log('halo');
+}
+var obj1 = new Halo();
+var obj2 = new Halo();
+// this mengembalikan object yang baru dibuat
+
+
+
+// ========================== //
+
+// this
+// console.log(this);
